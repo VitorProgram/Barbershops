9ee39a84c6f032ea9d7a787459756003df0abca3
João Vitor <joaovitornascimentoif@gmail.com>
2024-10-12 16:11:57 -0300

    chore: add color theme variables

--- src/styles/Globals/Global.tsx
+++ src/styles/Globals/Global.tsx
@@ -4,6 +4,19 @@
 import { createGlobalStyle } from "styled-components"
 
 const Global = createGlobalStyle`
+    :root {
+        --primary-purple: #8162ff;
+        --dark-purple: #221c3d;
+        --background-black: #141518;
+        --secondary-black: #1a1b1f;
+        --gray-one: #26272b;
+        --gray-two: #4e525b;
+        --gray-three: #839996; 
+        --white: #fff;
+        --red: #ef4444;
+        --dark-red: #2f1f1f;
+    }
+
 	* {
 		margin: 0;
 		padding: 0;	
